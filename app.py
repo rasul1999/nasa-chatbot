bfd69b981a7a06db209b6d5f8f917b19d31f2cf6
Rasul <rasulnabiyev59@gmail.com>
2019-10-19 12:51:52 +0400

    Image recognition

--- app.py
+++ app.py
@@ -2,19 +2,64 @@
 import random
 from flask import Flask, request
 from pymessenger.bot import Bot
-import os 
+
+import os
+import urllib.request
+
+
 app = Flask(__name__)
 
 ACCESS_TOKEN = 'EAAiZCfSvRvfYBANMBt3E5EqDckFrPEG1BvRQeJ8emrh9mmHbGYQjFVmgdln9jZBcZAtZBNo2bqsZCobmZBzFLTVzWSmNPZBK12WPlWS3ZAiKx1DWAB5wZCLeamA8yqZCjs7763EY9S0PeRN1lNrHx0CM3ZBO5gIfD9J6K8ccYFYo9w77zhbRJGATJbtMV0EzY1jlOYZD'   #ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = 'Salam'   #VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot (ACCESS_TOKEN)
 
-#We will receive messages that Facebook sends our bot at this endpoint 
+
+
+def load_model():
+
+    with open(model.json, 'r') as model_file:
+        model_json = model_file.read()
+
+    model = model_from_json(model_json)
+
+    model.load_weights('model.h5')
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+
+
+def predict_class(model, image_path):
+
+    image = image.load_img(image_path, target_size=(64, 64))
+    image = image.img_to_array(test_image)
+    image = np.expand_dims(test_image, axis=0)
+    result = classifier.predict(test_image)
+    
+    max_pred, max_class = 0, 0
+
+    for i in range(len(result)):
+        if pred > max_pred:
+            max_pred = pred
+            max_class = i
+
+    if max_class == 0:
+        return 'other'
+    elif max_class == 1:
+        return 'wild_fire'
+    else:
+        return 'residential_fire'
+
+
+model = load_model()
+
+
+#We will receive messages that Facebook sends our bot at this endpoint
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
-        that confirms all requests that your bot receives came from Facebook.""" 
+        that confirms all requests that your bot receives came from Facebook."""
         token_sent = request.args.get("hub.verify_token")
         return verify_fb_token(token_sent)
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
@@ -28,12 +73,15 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    response_sent_text = 'Salam siz metn mesaji gonderdiniz'
+                    response_sent_text = 'Please send media file showing fire accident'
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
-                    response_sent_nontext = 'Salam siz fayl gonderdiniz'
-                    send_message(recipient_id, response_sent_nontext)
+                    response_sent_nontext = 'Thanks for informing us. We will make feedback as soon as possible'
+                    image_url = message['message'].get('attachments')[0].payload.url
+                    urllib.request.urlretrieve(image_url, "image.jpg")
+                    image_class = predict_class(model, 'image.jpg')
+                    send_message(recipient_id, image_class)
     return "Message Processed"
 
 
@@ -44,12 +92,6 @@ def verify_fb_token(token_sent):
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
-
-#chooses a random message to send to the user
-def get_message():
-    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
-    # return selected item to the user
-    return random.choice(sample_responses)
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
